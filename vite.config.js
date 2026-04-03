72e935d258cf4102e42df9e2542ad836cb1ff71d
Railkrut <dzhienbulatov@bk.ru>
2026-04-04 00:06:17 +0300

    edited for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3020,
     open: true,
     cors: true,
     strictPort: true,
   },
-  base: './',
+  base: "/",
 });
